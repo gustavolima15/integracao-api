1b208711141d07a923e290721708027e689e3704
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-07-02 21:42:40 -0300

    Informações no corpo da requisição com Axios

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const app = express();
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-    const resultados = await axios.get('http://localhost:3001/carros');
+    //const resultados = await axios.get('http://localhost:3001/carros');
+    const novoCarro = {
+        modelo: 'Fusca',
+        marca: 'VW'
+    }
+    const resultados = await axios.post('http://localhost:3001/carros', novoCarro)
+
     return res.json(resultados.data);
 });
 
