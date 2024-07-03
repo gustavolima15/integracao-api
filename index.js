9887c88ae30d5eb03515e46de7dc7f27171f53d8
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-07-02 21:34:55 -0300

    Integração entre duas API's com Axios

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const axios = require('axios');
+
 const app = express();
 
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-    res.json('Olá API 1, porta 3000')
+    const resultados = await axios.get('http://localhost:3001/carros');
+    return res.json(resultados.data);
 });
 
 app.listen(3000);
